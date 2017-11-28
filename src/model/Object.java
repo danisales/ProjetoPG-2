73feaab5fab0e65d47b2741ad24d419501144320
Danielle Sales <dso@cin.ufpe.br>
2017-11-28 19:19:30 -0300

    Stop discarding point's world coordinates

--- src/model/Object.java
+++ src/model/Object.java
@@ -14,10 +14,15 @@ public class Object {
 	public ArrayList<Triangle> triangles;
 	public ArrayList<Vector> normalVectorsPts;
 	
+	public ArrayList<Point> viewCoordPoints;
+	public ArrayList<Triangle> viewCoordTriangle;
+	
 	public Object(String filepath, Camera c){
 		this.points = new ArrayList<Point>();
 		this.triangles = new ArrayList<Triangle>();
-		
+		this.viewCoordPoints = new ArrayList<Point>();
+		this.viewCoordTriangle = new ArrayList<Triangle>();
+
 		try {
 			FileReader fr = new FileReader(new File(filepath));
 			BufferedReader br = new BufferedReader(fr);
@@ -28,7 +33,8 @@ public class Object {
 			
 			for(int i = 0; i < nbPoints; i++){
 				Point p = FileHandler.getPoint(br.readLine());
-				points.add(c.worldToView(p));
+				points.add(p);
+				viewCoordPoints.add(c.worldToView(p));
 			}
 			
 			for(int i = 0; i < nbTriangles; i++){
@@ -40,6 +46,13 @@ public class Object {
 				
 				Triangle t = new Triangle(p1, p2, p3);
 				triangles.add(t);
+				
+				p1 = viewCoordPoints.get(pts[0]-1);
+				p2 = viewCoordPoints.get(pts[1]-1);
+				p3 = viewCoordPoints.get(pts[2]-1);
+				
+				t = new Triangle(p1, p2, p3);
+				viewCoordTriangle.add(t);
 			}
 			
 			br.close();
