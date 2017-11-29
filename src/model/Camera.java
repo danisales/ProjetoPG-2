9f790dfeab036160857c9f5715d8a12501f320c6
Danielle Sales <dso@cin.ufpe.br>
2017-11-28 23:00:12 -0300

    Fix change of coordinates

--- src/model/Camera.java
+++ src/model/Camera.java
@@ -11,7 +11,6 @@ public class Camera {
 	public double d, hx, hy;
 	public Vector V, N, U;
 	public Point C;
-	public double[][] changeOfBasisMatrix;
 	
 	public Camera(String filepath){
 		try {
@@ -36,28 +35,9 @@ public class Camera {
 		this.N = BasicOperations.normalize(this.N);
 		this.V = BasicOperations.normalize(BasicOperations.gramSchmidt(V, N));
 		this.U = BasicOperations.vectorProduct(N, V);
-		this.changeOfBasisMatrix = this.changeOfBasisMatrix();
 	}
 	
 	public Point worldToView(Point p){
-		Point subPt = BasicOperations.subPoints(p, this.C);
-		double[][] sub = {{subPt.x, subPt.y, subPt.z}};
-		Matrix mult;
-		double[] point = new double[3];
-		try {
-			mult = Matrix.multiply(this.changeOfBasisMatrix, sub);
-			point = mult.matrix[0];
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new Point(point[0], point[1], point[2]);
-	}
-	
-	private double[][] changeOfBasisMatrix(){
-		double[] U = {this.U.x, this.U.y, this.U.z};
-		double[] V = {this.V.x, this.V.y, this.V.z};
-		double[] N = {this.N.x, this.N.y, this.N.z};
-		double[][] M = {U, V, N};
-		return M;
+		return BasicOperations.subPoints(p, this.C);
 	}
 }
