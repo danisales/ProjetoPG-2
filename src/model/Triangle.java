59d6de79d3acddd8fc3fa6c78d5e20d74669bf4f
albertmourato <ams11@cin.ufpe.br>
2017-11-30 00:04:43 -0300

    sorting triangle points and getXmin/max

--- src/model/Triangle.java
+++ src/model/Triangle.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 import util.BasicOperations;
 
 public class Triangle {
@@ -22,5 +25,55 @@ public class Triangle {
 		Vector CA = new Vector(this.p3.x - this.p1.x,this.p3.y - this.p1.y,this.p3.z - this.p1.z);
 		Vector normal = BasicOperations.vectorProduct(BA, CA);
 		return BasicOperations.normalize(normal);
+	}
+	
+	public static ArrayList <Point> sortPointsOfTriangle(Triangle v) {
+		ArrayList <Point> points = new ArrayList<Point>();
+		points.add(v.p1);
+		points.add(v.p2);
+		points.add(v.p3);
+		points.sort(new Comparator<Point>() {
+			public int compare(Point p1, Point p2) {
+				return p1.y != p2.y? ((p1.y < p2.y)? 1:0) : ((p1.x < p2.x)?1:0);
+			}
+		});
+		return points;
+	}
+	
+	
+	public static double getAlfa(Point p1, Point p2) {
+		if(p1.x==p2.x)return -1; //Pontos no mesmo X
+		if(p1.y==p2.y)return -2; // Pontos no mesmo Y
+		
+		return (p2.y - p1.y)/(p2.x - p1.x);
+	}
+	
+	//Usa o alfa de P1 e P2
+	public static double getXMin(double currentX, double alfa) {
+		return currentX+(1/alfa);
+	}
+	
+	//esta exatamente igual ao Xmin, mas separei pra voce entender. A diferenca e que usaremos o alfa de p2 e p3
+	public static double getXMax(double currentX, double alfa) {
+		return currentX+(1/alfa);
+	}
+	
+	public static ArrayList<Triangle> splitTriangle(Triangle t){
+		//TODO
+		
+		return null;
+	}
+	
+	public static void main(String args[]) {
+		Triangle t = new Triangle(new Point(10,20,30), new Point(40,20,60), new Point(80,90,100));
+		ArrayList <Point> points = new ArrayList<Point>();
+		points.add(t.p2);
+		points.add(t.p3);
+		points.add(t.p1);
+		points = sortPointsOfTriangle(t);
+		
+		for(Point p : points) {
+			System.out.println("X: "+p.x+" Y: "+p.y);
+		}
 	}
 }
